752c3c1a9d5eab093e4ce66452674b07f8aafdb9
ethanriahi <41567909+ethanriahi@users.noreply.github.com>
2023-12-05 11:32:41 +0100

    Revert "CRUD CONTACT"

--- src/main/java/com/example/projetFsr/controller/ContactController.java
+++ src/main/java/com/example/projetFsr/controller/ContactController.java
@@ -1,7 +1,6 @@
 package com.example.projetFsr.controller;
 
 import com.example.projetFsr.model.Contact;
-import com.example.projetFsr.model.ContactDTO;
 import com.example.projetFsr.model.PhoneNumber;
 import com.example.projetFsr.service.ServiceContact;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,19 +20,5 @@ public class ContactController {
         Integer idContact = contact.getIdContact();
         contact.setIdContact(idContact);
         return serviceContact.createContact(contact);
-    }
-
-    @RequestMapping(method = RequestMethod.PUT, value = "/deleteContact")
-    public void deleteContact(@RequestParam Integer idContact){
-        serviceContact.deleteContact(idContact);
-    }
-    @GetMapping("/getContactInfo")
-    public List<ContactDTO> getContactInfo(@RequestParam Integer idContact){
-        return serviceContact.getContactInfo(idContact);
-    }
-
-    @RequestMapping(method = RequestMethod.PUT, value = "/updateContact")
-    public void updateContact(@RequestBody Contact contact){
-        serviceContact.updateContact(contact);
     }
 }
